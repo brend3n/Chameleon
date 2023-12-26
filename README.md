781479bf858be0a1eea22fe8155c77bae54ec77e
brenden <brenden@pop-os.localdomain>
2023-12-25 19:04:10 -0500

    Added command line args instead of menu only and updated readme

--- README.md
+++ README.md
@@ -1,10 +1,85 @@
-# MAC Manager
+# MAC Address Changer
 
-This is a shell tool that allows the user to be able to easily change change or modify the MAC address on different network interfaces. This tool also manages changing and restoring original MAC addresses.
+## Overview
 
+The MAC Address Changer is a command-line tool that allows users to change their MAC address on a specified network interface. This tool is useful for scenarios where users need to modify their device's MAC address for privacy, security, or network troubleshooting purposes.
 
-# Installation
-sudo apt install macmanager
-# Setup
+## Features
 
-# Notes
+- Display the current MAC address of a network interface.
+- Change the MAC address to a random value.
+- Set a specific MAC address provided by the user.
+- Save and restore the original MAC address.
+- View the history of MAC address changes.
+
+## Prerequisites
+
+- **Operating System:** Linux
+- **Permissions:** The tool requires root or superuser permissions to modify network interface settings.
+
+## Usage
+
+There are two ways to use this tool:
+1. Command line arguments
+2. Menu
+
+## Command Line
+
+```bash
+sudo ./mac-changer.sh <interface>
+```
+### Commands
+
+1. ### **Display Current MAC Address**
+    ```bash
+    ./mac-changer.sh -d <interface>
+    ```
+2. ### **Change to Random MAC Address**
+    ```bash
+    ./mac-changer.sh -r <interface>
+    ```
+3. ### **Change to Specific MAC Address**
+    ```bash
+    ./mac-changer.sh -m <new_mac_address> -i <interface>
+    ```
+4. ### **Restore Original MAC Address**
+    ```bash
+    ./mac-changer.sh -o <interface>
+    ```
+5. ### **Display MAC Address Change History**
+    ```bash
+    ./mac-changer.sh -h
+    ```
+## Menu Mode
+
+```bash
+sudo ./mac-changer.sh <interface>
+```
+
+Replace \<interface> with the actual network interface you want to modify.
+
+### Menu Options
+1. **Display  current MAC address:** 
+    - Shows the current MAC address of the specified network interface.
+2. **Change to random MAC address:**
+    - Sets a random MAC address for the specified network interface.
+3. **Change to specific MAC address:** 
+    - Allows the user to enter a specific MAC address for the network interface.
+4. **Restore original MAC address:** 
+    - Restores the original MAC address for the specified network interface.
+5. **Display MAC address change history:** 
+    - Shows the history of MAC address changes.
+6. **Exit:** 
+    -   Exits the script.
+
+## Example
+```bash
+sudo ./mac-changer.sh eth0
+```
+
+## License
+This tool is distributed under the MIT License.
+
+## Disclaimer
+
+Changing your MAC address may affect your network connectivity. Use this tool responsibly and ensure compliance with applicable laws and regulations.
--- macmanager.sh
+++ macmanager.sh
@@ -9,6 +9,20 @@ OG_MAC_FILE="./.original_mac_write_only"
 # File to store the original MAC address and flag
 OG_MAC_FILE_FLAG="./.original_mac_write_once.flag"
 
+# Assign the provided interface to a variable
+interface=$1
+
+display_help() {
+    echo "Usage: $0 <options>"
+    echo "Options:"
+    echo "  -d <interface>        Display current MAC address"
+    echo "  -r <interface>        Change to random MAC address"
+    echo "  -m <new_mac_address> -i <interface>  Change to specific MAC address"
+    echo "  -o <interface>        Restore original MAC address"
+    echo "  -p                    Display history"
+    echo "  -h                    Display this help message"
+}
+
 # Function to display the current MAC address
 display_current_mac() {
     current_mac=$(ifconfig $interface | awk '/ether/ {print $2}')
@@ -46,8 +60,7 @@ change_to_specific_mac() {
 save_original_mac() {
     # Check if the flag file exists
     if [ -e "$OG_MAC_FILE_FLAG" ]; then
-        echo "Original MAC address already saved. Cannot save again."
-        echo "Delete $OG_MAC_FILE_FLAG if you really need to change it"
+        echo "Original MAC address saved."
         return
     fi
 
@@ -59,7 +72,7 @@ save_original_mac() {
     touch $OG_MAC_FILE_FLAG
 
     # Display a confirmation message
-    echo "Original MAC address <${original_mac}>saved to $OG_MAC_FILE (write-only)."
+    echo "Original MAC address <${original_mac}> saved to $OG_MAC_FILE (write-only)."
 
     # Set permissions to make the file writable only once
     chmod a-w "$OG_MAC_FILE"
@@ -120,6 +133,7 @@ change_to_random_mac() {
 # Check if the script is run as root
 if [[ $EUID -ne 0 ]]; then
     echo "This script must be run as root."
+    echo "Usage: sudo $0 <interface>"
     exit 1
 fi
 
@@ -130,38 +144,117 @@ fi
 
 # Check if the network interface is provided as an argument
 if [ -z "$1" ]; then
-    echo "Usage: $0 <interface>"
+    echo "Usage: sudo $0 <interface>"
     exit 1
 fi
 
-# Assign the provided interface to a variable
-interface=$1
 
-# Infinite loop
-while true; do
-    # Display the menu
-    echo "1. Display current MAC address"
-    echo "2. Change to random MAC address"
-    echo "3. Change to a specific MAC address"
-    echo "4. Save original MAC address to a write-only file"
-    echo "5. Restore original MAC address"
-    echo "6. Display MAC address change history"
-    echo "7. Exit"
+# Save the original MAC address if its the first time running the script
+save_original_mac
 
-    # Read user choice
-    read -p "Enter your choice (1-7): " choice
+do_menu_mode() {
+    # Infinite loop
+    while true; do
+        # Display the menu
+        echo "1. Display current MAC address"
+        echo "2. Change to random MAC address"
+        echo "3. Change to a specific MAC address"
+        echo "4. Restore original MAC address"
+        echo "5. Display MAC address change history"
+        echo "6. Exit"
 
-    echo -e "\n\n"
-    # Perform the selected action
-    case $choice in
-        1) display_current_mac ;;
-        2) change_to_random_mac ;;
-        3) change_to_specific_mac ;;
-        4) save_original_mac ;;
-        5) restore_original_mac ;;
-        6) display_mac_history ;;
-        7) exit 0 ;;
-        *) echo "Invalid choice. Please try again." ;;
-    esac
-    echo -e "\n\n"
+        # Read user choice
+        read -p "Enter your choice (1-6): " choice
+
+        echo -e "\n\n"
+        # Perform the selected action
+        case $choice in
+            1) display_current_mac ;;
+            2) change_to_random_mac ;;
+            3) change_to_specific_mac ;;
+            4) restore_original_mac ;;
+            5) display_mac_history ;;
+            6) exit 0 ;;
+            *) echo "Invalid choice. Please try again." ;;
+        esac
+        echo -e "\n\n"
+    done   
+}
+
+
+while getopts ":d:hpr:o:m:i:" opt; do
+    case $opt in
+        d)
+            flag_d=true
+            interface="$OPTARG"
+            ;;
+        h)
+            flag_h=true
+            ;;
+        p)
+            flag_p=true          
+            ;;
+        r)
+            flag_r=true
+            interface="$OPTARG"
+            ;;
+        m)
+            flag_m=true
+            new_mac_addr="$OPTARG"
+            ;;
+        o)
+            flag_o=true
+            interface="$OPTARG"
+            ;;
+        i)
+            flag_i=true
+            interface="$OPTARG"
+            ;;
+        \?)
+            echo "Invalid option: -$OPTARG"
+            display_help
+            exit 1
+            ;;
+        :)
+            echo "Option -$OPTARG requires an argument."
+            display_help
+            exit 1
+            ;;
+    esac    
 done
+
+# Else, no options so we doin menu mode
+if [ "$flag_d" = true ] || \
+   [ "$flag_h" = true ] || \
+   [ "$flag_p" = true ] || \
+   [ "$flag_r" = true ] || \
+   [ "$flag_m" = true ] || \
+   [ "$flag_o" = true ] || \
+   [ "$flag_i" = true ]; then
+    : # no-op
+else
+    echo -e "Menu mode"
+    do_menu_mode
+fi
+
+if [ "$flag_d" = true ]; then
+    display_current_mac
+elif [ "$flag_r" = true ]; then
+    change_to_random_mac
+elif [ "$flag_o" = true ]; then
+    restore_original_mac
+elif [ "$flag_p" = true ]; then
+    display_mac_history
+elif [ "$flag_h" = true ]; then
+    display_help
+elif [ "$flag_m" = true ]; then
+    if [ "$flag_i" = true ]; then
+        change_to_specific_mac
+    else
+        echo -e "Missing arg for interface"
+    fi 
+else
+    echo -e "Invalid command"
+    display_help
+    exit 1
+fi
